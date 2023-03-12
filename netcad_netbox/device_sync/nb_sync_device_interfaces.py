42998740a2db55aa167cd13cbd1a1a9264c48c2b
Jeremy Schulman <nwkautomaniac@gmail.com>
2023-03-12 12:23:53 -0400

    updates

--- netcad_netbox/device_sync/nb_sync_device_interfaces.py
+++ netcad_netbox/device_sync/nb_sync_device_interfaces.py
@@ -5,7 +5,10 @@ from httpx import Response
 
 from netcad.logger import get_logger
 from netcad.device import Device
+
 from netcad_netbox.aionetbox import NetboxClient
+from netcad_netbox.netbox_map_if_type import netbox_map_interface_type
+from netcad_netbox.netbox_design_config import NetBoxInterfaceProperties
 
 
 async def nb_sync_device_interface_objs(
@@ -25,13 +28,22 @@ async def nb_sync_device_interface_objs(
     add_if_names = expd_if_names - has_if_names
     unx_if_names = has_if_names - expd_if_names  # unexpected
 
-    await _delete_interfaces(nb_api, del_if_recs=map(nb_if_name_map.get, unx_if_names))
+    await _delete_interfaces(
+        nb_api, dev, del_if_recs=map(nb_if_name_map.get, unx_if_names)
+    )
     await _create_interfaces(
         nb_api, dev, nb_dev_id=nb_dev_rec["id"], add_if_names=add_if_names
     )
     await _sync_existing_interfaces(
         nb_api, dev, chk_if_recs=map(nb_if_name_map.get, chk_if_names)
     )
+
+
+# -----------------------------------------------------------------------------
+#
+#                    Create new NetBox Interface Records
+#
+# -----------------------------------------------------------------------------
 
 
 async def _create_interfaces(
@@ -42,15 +54,7 @@ async def _create_interfaces(
 
     for if_name in add_if_names:
         if_obj = dev.interfaces[if_name]
-        if_prof = if_obj.profile
-        if if_prof.is_lag:
-            if_type = "lag"
-        elif if_prof.is_loopback:
-            if_type = "virtual"
-        elif if_prof.is_virtual:
-            if_type = "virtual"
-        else:
-            if_type = "other"
+        if_type = netbox_map_interface_type(if_obj)
 
         new_if_body = dict(
             device=nb_dev_id,
@@ -69,11 +73,85 @@ async def _create_interfaces(
         new_if_map[if_name] = res.json()
 
 
-async def _delete_interfaces(nb_api: NetboxClient, del_if_recs: Iterable[dict]):
-    pass
+# -----------------------------------------------------------------------------
+#
+#                    Update existing NetBox Interface Records
+#
+# -----------------------------------------------------------------------------
 
 
 async def _sync_existing_interfaces(
     nb_api: NetboxClient, dev: Device, chk_if_recs: Iterable[dict]
+) -> dict[str, dict]:
+    """
+    This function updates any interfaces that needs to be updated.  Any updated
+    interface records are returned.
+
+    Parameters
+    ----------
+    nb_api
+        The instance to the NetBox API
+
+    dev
+        The design device
+
+    chk_if_recs
+        The list of NetBox interface records that will be used to check against the
+        design device interfaces
+    """
+    log = get_logger()
+
+    updated_if_recs = dict()
+
+    for nb_if_rec in chk_if_recs:
+        if_name = nb_if_rec["name"]
+        dsn_if_obj = dev.interfaces[if_name]
+
+        if_props_has = NetBoxInterfaceProperties(
+            description=nb_if_rec["description"],
+            enabled=nb_if_rec["enabled"],
+            if_type=nb_if_rec["type"]["value"],
+        )
+
+        if_props_epx = NetBoxInterfaceProperties(
+            description=dsn_if_obj.desc,
+            if_type=netbox_map_interface_type(dsn_if_obj),
+            enabled=dsn_if_obj.enabled,
+        )
+
+        if if_props_epx == if_props_has:
+            # no change
+            continue
+
+        patch_if_body = dict(
+            name=if_name,
+            type=if_props_epx.if_type,
+            description=if_props_epx.description,
+            enabled=if_props_epx.enabled,
+        )
+
+        res = await nb_api.op.dcim_interfaces_partial_update(
+            id=nb_if_rec["id"], json=patch_if_body
+        )
+
+        if res.status_code != HTTPStatus.OK:
+            log.error(f"{dev.name}:{if_name} update failed: {res.text}")
+            continue
+
+        log.info(f"{dev.name}: {if_name}: updated OK")
+        updated_if_recs[if_name] = res.json()
+
+    return updated_if_recs
+
+
+# -----------------------------------------------------------------------------
+#
+#                 Delete NetBox Interface Records not in design
+#
+# -----------------------------------------------------------------------------
+
+
+async def _delete_interfaces(
+    nb_api: NetboxClient, dev: Device, del_if_recs: Iterable[dict]
 ):
     pass
--- netcad_netbox/netbox_design_config.py
+++ netcad_netbox/netbox_design_config.py
@@ -1,6 +1,17 @@
+from typing import Optional
+
 from pydantic.dataclasses import dataclass
+from pydantic import Field
 from netcad.design import Design
 from netcad.device import Device
+
+
+def find_mismatched_fields(me, other):
+    return {
+        field
+        for field in getattr(me, "__dataclass_fields__")
+        if getattr(me, field) != getattr(other, field)
+    }
 
 
 @dataclass
@@ -14,11 +25,19 @@ class NetBoxDeviceProperties:
     primary_ip: str
 
     def __sub__(self, other: "NetBoxDeviceProperties") -> set[str]:
-        return {
-            field
-            for field in self.__dataclass_fields__
-            if getattr(self, field) != getattr(other, field)
-        }
+        return find_mismatched_fields(self, other)
+
+
+@dataclass
+class NetBoxInterfaceProperties:
+    enabled: bool
+    description: str
+    if_type: str
+    tags: Optional[list[str]] = Field(default_factory=list)
+    is_mgmt_only: Optional[bool] = Field(False)
+
+    def __sub__(self, other: "NetBoxInterfaceProperties") -> set[str]:
+        return find_mismatched_fields(self, other)
 
 
 class NetBoxDesignConfig:
